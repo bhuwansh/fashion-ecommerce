6143674af1eebdc68c444a08b32291a24dbc3bbd
kritika suthar <kritikasuthar25@gmail.com>
2024-04-30 18:33:19 +0530

    connection.php code update

--- admin/connection.inc.php
+++ admin/connection.inc.php
@@ -2,9 +2,9 @@
 session_start();
 $con=mysqli_connect("localhost","root","","myproject");
 define('SERVER_PATH','http://localhost/fashion-ecommerce/admin/');
-define('SITE_PATH','http://localhost/fashion-ecommerce/');
-define('SITE_PATH','');
+define('SITE_PATH','http://localhost/fashion-ecommerce/ecommerce-frontend/');
+// define('SITE_PATH','');
 
-define('PRODUCT_IMAGE_SERVER_PATH',$SERVER_PATH.'/media/products');
-define('PRODUCT_IMAGE_SITE_PATH',$SITE_PATH.'/media/products');
+define('PRODUCT_IMAGE_SERVER_PATH',$SITE_PATH.'images/product/');
+define('PRODUCT_IMAGE_SITE_PATH',$SITE_PATH.'images/product/');
 ?>
